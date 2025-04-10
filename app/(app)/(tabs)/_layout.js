006089833106a3124498ccb23c4cb0a91ea74be2
jbucaria <157171759+jbucaria@users.noreply.github.com>
2025-04-10 09:48:24 -0400

    app working after 18.4 update

--- app/(app)/(tabs)/_layout.js
+++ app/(app)/(tabs)/_layout.js
@@ -1,82 +1,53 @@
 import { Tabs } from 'expo-router'
-import React, { useEffect } from 'react'
-import { Platform, StyleSheet, StatusBar, View } from 'react-native'
-import HapticTab from '@/components/ui/HapticTab'
-import { IconSymbol } from '@/components/ui/IconSymbol'
-import { BlurView } from 'expo-blur'
-import { Colors } from '@/constants/Colors'
-import { useColorScheme } from '@/hooks/useColorScheme'
+import React from 'react'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme()
-
-  useEffect(() => {
-    if (Platform.OS === 'android') {
-      StatusBar.setTranslucent(true)
-      StatusBar.setBackgroundColor('transparent')
-    }
-  }, [])
-
-  const BlurTabBarBackground = () => (
-    <BlurView tint="light" intensity={50} style={StyleSheet.absoluteFill} />
-  )
-
-  const SolidTabBarBackground = () => (
-    <View style={[StyleSheet.absoluteFill, { backgroundColor: '#fff' }]} />
-  )
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: '#007AFF',
-        tabBarInactiveTintColor: Colors[colorScheme ?? 'light'].tabIconDefault,
-        headerShown: false,
-        tabBarButton: props => <HapticTab {...props} />,
-        tabBarBackground: Platform.select({
-          ios: BlurTabBarBackground,
-          android: SolidTabBarBackground,
-        }),
-        tabBarStyle: {
-          position: 'absolute',
-        },
-      }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
-          ),
+    <SafeAreaProvider>
+      <Tabs
+        screenOptions={{
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="quickBooks"
-        options={{
-          title: 'Acct.',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="dollarsign.circle" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="maps"
-        options={{
-          title: 'Map',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="map.circle" color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Profile',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="gear" color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: 'Home',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="home" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="quickBooks"
+          options={{
+            title: 'Acct.',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="cash-outline" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="maps"
+          options={{
+            title: 'Map',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="map-outline" size={28} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="settings"
+          options={{
+            title: 'Profile',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="settings-outline" size={28} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaProvider>
   )
 }
